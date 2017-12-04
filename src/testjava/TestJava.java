239b1302429bcc709bebb39de9c00dc132eff758
Rubesh Shankhadev <rubesh.shankhadev@national.kw.com>
2017-12-03 22:18:39 -0600

    First Commit

---
+++ src/testjava/TestJava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjava;
+
+/**
+ *
+ * @author rubesh.shankhadev
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is test project");
+    }
+}
